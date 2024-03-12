b79200e89f01a3303b22b5172a0d56543dbc3ff7
SkaggyWallis <163072823+SkaggyWallis@users.noreply.github.com>
2024-03-12 00:52:57 -0300

    Update config.rs

--- dalstPURRR/project-files/config.rs
+++ dalstPURRR/project-files/config.rs
@@ -18,21 +18,26 @@ pub struct TokenConfig {
 }
 
 impl TokenConfig {
-    pub fn new(mint_pubkey: Pubkey) -> Self {
-        Self {
+    pub fn new(
+        mint_authority_str: &str,
+        admin_str: &str,
+        freeze_authority_str: &str,
+    ) -> Result<Self, PubkeyError> {
+        Ok(Self {
             name: "Meowdolf Kittler".to_string(),
             symbol: "KTLR".to_string(),
             decimals: 9,
             initial_supply: 1_000_000_000,
-            mint_authority: mint_pubkey,
-            admin: Pubkey::new_unique(),
-            freeze_authority: Pubkey::new_unique(),
+            mint_authority: Pubkey::new_from_str(9o2PFUYDTtTESRVuhiNgcsNPnCENJ3psuhNGP59qsSR4)?,
+            admin: Pubkey::new_from_str(9yUSYzkfRABKj967hZ2avrAzcSVacrhvXJDQWCBFtheA)?,
+            freeze_authority: Pubkey::new_from_str(9cuXDkEa32UXAq1TkGZPQeYu1osjxdoguyCpvDGRUeSn)?,
             base_uri: "https://example.com/api/tokens/".to_string(),
             seller_fee_basis_points: 500,
             image_url: "https://drive.google.com/file/d/1z17iO5THVHSd2vykIx8qbZFOQy0_-t97/view?usp=drive_link".to_string(),
             max_total_supply: 1_000_000_000_000,
             wallet_mint_cap: 10_000,
             yearly_mint_cap: 1_000_000_000,
-        }
+        })
     }
 }
+
